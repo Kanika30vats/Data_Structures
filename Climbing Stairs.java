c49bbe8dea362cca469465b0901c36111fc4d72e
Kanika Sharma <kanika30vats@gmail.com>
2024-01-18 09:55:59 +0530

    70 Leetcode Easy

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -8,10 +8,8 @@ class Solution {
     }
 
     private int dp(int n, int memo[]) {
-        if(n == 0)
+        if(n <= 1)
             return 1;
-        if(n < 0)
-            return 0;
 
         if(memo[n] != -1)
             return memo[n];
