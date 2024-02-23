7e6850b3425d7673da561e696edbbf397dfbe5e7
Kanika Sharma <kanika30vats@gmail.com>
2024-02-23 14:25:36 +0530

    121 Leetcode Easy

--- Best Time to Buy and Sell Stock.java
+++ Best Time to Buy and Sell Stock.java
@@ -4,7 +4,7 @@ class Solution {
         int currProfit = 0;
         int minElement = prices[0];
         for(int i = 1; i < prices.length; i++) {
-            currProfit = Math.max(prices[i] - minElement, currProfit);
+            currProfit = prices[i] - minElement;
             profit = Math.max(currProfit, profit);
             minElement = Math.min(prices[i], minElement);
         }
